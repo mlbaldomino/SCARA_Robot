4e9f64fe27349811cca85584502da35941afcb33
Martha Linares <mlinaresb@outlook.com>
2023-10-08 16:21:55 -0600

    Improve code

--- RobotFirmware/RobotFirmware.ino
+++ RobotFirmware/RobotFirmware.ino
@@ -8,82 +8,106 @@
 #include <Servo.h>
 #include <math.h>
 
-#define limitSwitch1 11
-#define limitSwitch2 10
-#define limitSwitch3 9
-#define limitSwitch4 A3
+#define MAX_STEPPER_SPEED 4000
+#define STEPPER_ACCELERATION 2000
+
+#define LIMIT_SWITCH_PIN_1 11
+#define LIMIT_SWITCH_PIN_2 10
+#define LIMIT_SWITCH_PIN_3 9
+#define LIMIT_SWITCH_PIN_4 A3
 
 // Define the stepper motors and the pins the will use
-AccelStepper stepper1(1, 2, 5); // (Type:driver, STEP, DIR)
+// (Type:driver, STEP, DIR)
+AccelStepper stepper1(1, 2, 5);
 AccelStepper stepper2(1, 3, 6);
 AccelStepper stepper3(1, 4, 7);
 AccelStepper stepper4(1, 12, 13);
 
-Servo gripperServo;  // create servo object to control a servo
-
+// Create servo object to control a servo
+Servo gripperServo;
 
 double x = 10.0;
 double y = 10.0;
-double L1 = 228; // L1 = 228mm
-double L2 = 136.5; // L2 = 136.5mm
+double L1 = 228.0;  // L1 = 228.0 mm
+double L2 = 136.5;  // L2 = 136.5 mm
 double theta1, theta2, phi, z;
 
 int stepper1Position, stepper2Position, stepper3Position, stepper4Position;
 
 const float theta1AngleToSteps = 44.444444;
 const float theta2AngleToSteps = 35.555555;
-const float phiAngleToSteps = 10;
-const float zDistanceToSteps = 100;
+const float phiAngleToSteps = 10.0;
+const float zDistanceToSteps = 100.0;
 
-byte inputValue[5];
+byte inputValue[5] = { 0 };
 int k = 0;
 
 String content = "";
-int data[10];
+/*
+ * 0
+ * 1
+ * 1
+ * 3
+ * 4
+ * 5 - z (heigth)
+ * 6 - grip value
+ * 7
+ * 8
+ * 9
+ */
+int data[10] = { 0 };
 
-int theta1Array[100];
-int theta2Array[100];
-int phiArray[100];
-int zArray[100];
-int gripperArray[100];
+int theta1Array[100] = { 0 };
+int theta2Array[100] = { 0 };
+int phiArray[100] = { 0 };
+int zArray[100] = { 0 };
+int gripperArray[100] = { 0 };
+
 int positionsCounter = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(limitSwitch1, INPUT_PULLUP);
-  pinMode(limitSwitch2, INPUT_PULLUP);
-  pinMode(limitSwitch3, INPUT_PULLUP);
-  pinMode(limitSwitch4, INPUT_PULLUP);
+  // Set internal pullups for input limit switch
+  pinMode(LIMIT_SWITCH_PIN_1, INPUT_PULLUP);
+  pinMode(LIMIT_SWITCH_PIN_2, INPUT_PULLUP);
+  pinMode(LIMIT_SWITCH_PIN_3, INPUT_PULLUP);
+  pinMode(LIMIT_SWITCH_PIN_4, INPUT_PULLUP);
 
-  // Stepper motors max speed
-  stepper1.setMaxSpeed(4000);
-  stepper1.setAcceleration(2000);
-  stepper2.setMaxSpeed(4000);
-  stepper2.setAcceleration(2000);
-  stepper3.setMaxSpeed(4000);
-  stepper3.setAcceleration(2000);
-  stepper4.setMaxSpeed(4000);
-  stepper4.setAcceleration(2000);
+  // Set stepper motors max speed
+  stepper1.setMaxSpeed(MAX_STEPPER_SPEED);
+  stepper2.setMaxSpeed(MAX_STEPPER_SPEED);
+  stepper3.setMaxSpeed(MAX_STEPPER_SPEED);
+  stepper4.setMaxSpeed(MAX_STEPPER_SPEED);
 
+  // Set stepper motors acceleration
+  stepper1.setAcceleration(STEPPER_ACCELERATION);
+  stepper2.setAcceleration(STEPPER_ACCELERATION);
+  stepper3.setAcceleration(STEPPER_ACCELERATION);
+  stepper4.setAcceleration(STEPPER_ACCELERATION);
+
+  // Configure gripper servo (pin, min, max)
   gripperServo.attach(A0, 600, 2500);
-  // initial servo value - open gripper
+
+  // Set initial servo value - open gripper
   data[6] = 180;
   gripperServo.write(data[6]);
+
   delay(1000);
+
   data[5] = 100;
-  homing();
+  setHome();
 }
 
 void loop() {
 
   if (Serial.available()) {
-    content = Serial.readString(); // Read the incomding data from Processing
+    content = Serial.readString();  // Read the incomding data from Processing
     // Extract the data from the string and put into separate integer variables (data[] array)
     for (int i = 0; i < 10; i++) {
-      int index = content.indexOf(","); // locate the first ","
-      data[i] = atol(content.substring(0, index).c_str()); //Extract the number from start to the ","
-      content = content.substring(index + 1); //Remove the number from the string
+      int index = content.indexOf(",");                     // locate the first ","
+      data[i] = atol(content.substring(0, index).c_str());  //Extract the number from start to the ","
+      content = content.substring(index + 1);               //Remove the number from the string
     }
     /*
      data[0] - SAVE button status
@@ -98,7 +122,7 @@ void loop() {
     */
     // If SAVE button is pressed, store the data into the appropriate arrays
     if (data[0] == 1) {
-      theta1Array[positionsCounter] = data[2] * theta1AngleToSteps; //store the values in steps = angles * angleToSteps variable
+      theta1Array[positionsCounter] = data[2] * theta1AngleToSteps;  //store the values in steps = angles * angleToSteps variable
       theta2Array[positionsCounter] = data[3] * theta2AngleToSteps;
       phiArray[positionsCounter] = data[4] * phiAngleToSteps;
       zArray[positionsCounter] = data[5] * zDistanceToSteps;
@@ -144,20 +168,19 @@ void loop() {
       }
       if (i == 0) {
         gripperServo.write(gripperArray[i]);
-      }
-      else if (gripperArray[i] != gripperArray[i - 1]) {
+      } else if (gripperArray[i] != gripperArray[i - 1]) {
         gripperServo.write(gripperArray[i]);
-        delay(800); // wait 0.8s for the servo to grab or drop - the servo is slow
+        delay(800);  // wait 0.8s for the servo to grab or drop - the servo is slow
       }
 
       //check for change in speed and acceleration or program stop
       if (Serial.available()) {
-        content = Serial.readString(); // Read the incomding data from Processing
+        content = Serial.readString();  // Read the incomding data from Processing
         // Extract the data from the string and put into separate integer variables (data[] array)
         for (int i = 0; i < 10; i++) {
-          int index = content.indexOf(","); // locate the first ","
-          data[i] = atol(content.substring(0, index).c_str()); //Extract the number from start to the ","
-          content = content.substring(index + 1); //Remove the number from the string
+          int index = content.indexOf(",");                     // locate the first ","
+          data[i] = atol(content.substring(0, index).c_str());  //Extract the number from start to the ","
+          content = content.substring(index + 1);               //Remove the number from the string
         }
 
         if (data[1] == 0) {
@@ -228,7 +251,10 @@ void loop() {
   stepper3.moveTo(stepper3Position);
   stepper4.moveTo(stepper4Position);
 
-  while (stepper1.currentPosition() != stepper1Position || stepper2.currentPosition() != stepper2Position || stepper3.currentPosition() != stepper3Position || stepper4.currentPosition() != stepper4Position) {
+  while (stepper1.currentPosition() != stepper1Position 
+      || stepper2.currentPosition() != stepper2Position 
+      || stepper3.currentPosition() != stepper3Position 
+      || stepper4.currentPosition() != stepper4Position) {
     stepper1.run();
     stepper2.run();
     stepper3.run();
@@ -241,16 +267,16 @@ void loop() {
 
 void serialFlush() {
   while (Serial.available() > 0) {  //while there are characters in the serial buffer, because Serial.available is >0
-    Serial.read();         // get one character
+    Serial.read();                  // get one character
   }
 }
 
-void homing() {
+void setHome() {
   // Homing Stepper4
-  while (digitalRead(limitSwitch4) != 1) {
+  while (digitalRead(LIMIT_SWITCH_PIN_4) != 1) {
     stepper4.setSpeed(1500);
     stepper4.runSpeed();
-    stepper4.setCurrentPosition(17000); // When limit switch pressed set position to 0 steps
+    stepper4.setCurrentPosition(17000);  // When limit switch pressed set position to 0 steps
   }
   delay(20);
   stepper4.moveTo(10000);
@@ -259,36 +285,34 @@ void homing() {
   }
 
   // Homing Stepper3
-  while (digitalRead(limitSwitch3) != 1) {
+  while (digitalRead(LIMIT_SWITCH_PIN_3) != 1) {
     stepper3.setSpeed(-1100);
     stepper3.runSpeed();
-    stepper3.setCurrentPosition(-1662); // When limit switch pressed set position to 0 steps
+    stepper3.setCurrentPosition(-1662);  // When limit switch pressed set position to 0 steps
   }
   delay(20);
-
   stepper3.moveTo(0);
   while (stepper3.currentPosition() != 0) {
     stepper3.run();
   }
 
   // Homing Stepper2
-  while (digitalRead(limitSwitch2) != 1) {
+  while (digitalRead(LIMIT_SWITCH_PIN_2) != 1) {
     stepper2.setSpeed(-1300);
     stepper2.runSpeed();
-    stepper2.setCurrentPosition(-5420); // When limit switch pressed set position to -5440 steps
+    stepper2.setCurrentPosition(-5420);  // When limit switch pressed set position to -5440 steps
   }
   delay(20);
-
   stepper2.moveTo(0);
   while (stepper2.currentPosition() != 0) {
     stepper2.run();
   }
 
   // Homing Stepper1
-  while (digitalRead(limitSwitch1) != 1) {
+  while (digitalRead(LIMIT_SWITCH_PIN_1) != 1) {
     stepper1.setSpeed(-1200);
     stepper1.runSpeed();
-    stepper1.setCurrentPosition(-3955); // When limit switch pressed set position to 0 steps
+    stepper1.setCurrentPosition(-3955);  // When limit switch pressed set position to 0 steps
   }
   delay(20);
   stepper1.moveTo(0);
